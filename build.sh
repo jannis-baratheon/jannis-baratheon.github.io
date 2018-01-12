5e821a76276cf2832034cdad3bcdf969a388c05d
Janusz Wiśniowski <wisniaaa@gmail.com>
2018-01-12 10:52:42 +0100

    updating paths

--- build.sh
+++ build.sh
@@ -1,40 +1,43 @@
 #!/bin/sh
 
+INTERMEDIATE_DIRECTORY=intermediate
+DIST_DIRECTORY=dist
+
 # clean
-rm -rf out
-rm -rf target
+rm -rf "${INTERMEDIATE_DIRECTORY}"
+rm -rf "${DIST_DIRECTORY}"
 
 # build resume
-node_modules/hackmyresume/dist/cli/index.js BUILD src/resume.json TO out/resume.html -t node_modules/jsonresume-theme-flat-custom
-node_modules/hackmyresume/dist/cli/index.js BUILD src/resume.json TO out/resume.pdf -t node_modules/jsonresume-theme-flat
-node_modules/hackmyresume/dist/cli/index.js BUILD src/resume.json TO out/resume.txt
-node_modules/hackmyresume/dist/cli/index.js BUILD src/resume.json TO out/resume.md
+node_modules/hackmyresume/dist/cli/index.js BUILD src/resume.json TO "${INTERMEDIATE_DIRECTORY}"/resume.html -t node_modules/jsonresume-theme-flat-custom
+node_modules/hackmyresume/dist/cli/index.js BUILD src/resume.json TO "${INTERMEDIATE_DIRECTORY}"/resume.pdf -t node_modules/jsonresume-theme-flat
+node_modules/hackmyresume/dist/cli/index.js BUILD src/resume.json TO "${INTERMEDIATE_DIRECTORY}"/resume.txt
+node_modules/hackmyresume/dist/cli/index.js BUILD src/resume.json TO "${INTERMEDIATE_DIRECTORY}"/resume.md
 
-# create target directory structure
-mkdir -p target/resumes
-mkdir -p target/css
-mkdir -p target/js
-mkdir -p target/vendor/css
-mkdir -p target/vendor/js
-mkdir -p target/vendor/fonts
+# create dist directory structure
+mkdir -p "${DIST_DIRECTORY}"/resumes
+mkdir -p "${DIST_DIRECTORY}"/css
+mkdir -p "${DIST_DIRECTORY}"/js
+mkdir -p "${DIST_DIRECTORY}"/vendor/css
+mkdir -p "${DIST_DIRECTORY}"/vendor/js
+mkdir -p "${DIST_DIRECTORY}"/vendor/fonts
 
-cp src/CNAME target
-cp src/index.html target
-cp src/style.css target/css
-cp src/script.js target/js
-cp out/resume.html out/resume.pdf out/resume.txt out/resume.md target/resumes
-./node_modules/bestzip/bin/cli.js target/resumes/resume.zip target/resumes/resume.*
+cp src/CNAME "${DIST_DIRECTORY}"
+cp src/index.html "${DIST_DIRECTORY}"
+cp src/style.css "${DIST_DIRECTORY}"/css
+cp src/script.js "${DIST_DIRECTORY}"/js
+cp "${INTERMEDIATE_DIRECTORY}"/resume.html "${INTERMEDIATE_DIRECTORY}"/resume.pdf "${INTERMEDIATE_DIRECTORY}"/resume.txt "${INTERMEDIATE_DIRECTORY}"/resume.md "${DIST_DIRECTORY}"/resumes
+./node_modules/bestzip/bin/cli.js "${DIST_DIRECTORY}"/resumes/resume.zip "${DIST_DIRECTORY}"/resumes/resume.*
 
 # css
-cp node_modules/bootstrap/dist/css/bootstrap.min.css target/vendor/css
+cp node_modules/bootstrap/dist/css/bootstrap.min.css "${DIST_DIRECTORY}"/vendor/css
 
 # js
-cp node_modules/bootstrap/dist/js/bootstrap.min.js target/vendor/js
-cp node_modules/jquery/dist/jquery.slim.min.js target/vendor/js
-cp node_modules/popper.js/dist/umd/popper.min.js target/vendor/js
+cp node_modules/bootstrap/dist/js/bootstrap.min.js "${DIST_DIRECTORY}"/vendor/js
+cp node_modules/jquery/dist/jquery.slim.min.js "${DIST_DIRECTORY}"/vendor/js
+cp node_modules/popper.js/dist/umd/popper.min.js "${DIST_DIRECTORY}"/vendor/js
 
 # fonts
-mkdir -p target/vendor/fonts/font-awesome
-cp -r node_modules/font-awesome/css node_modules/font-awesome/fonts target/vendor/fonts/font-awesome
-mkdir -p target/vendor/fonts/devicons
-cp -r node_modules/devicons/css node_modules/devicons/fonts target/vendor/fonts/devicons
+mkdir -p "${DIST_DIRECTORY}"/vendor/fonts/font-awesome
+cp -r node_modules/font-awesome/css node_modules/font-awesome/fonts "${DIST_DIRECTORY}"/vendor/fonts/font-awesome
+mkdir -p "${DIST_DIRECTORY}"/vendor/fonts/devicons
+cp -r node_modules/devicons/css node_modules/devicons/fonts "${DIST_DIRECTORY}"/vendor/fonts/devicons
