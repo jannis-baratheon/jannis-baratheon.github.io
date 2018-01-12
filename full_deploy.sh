65c24accb017b9b77d591b60082163a4eb790654
Janusz Wiśniowski <janusz@januszwisniowski.it>
2018-01-12 13:08:11 +0100

    Helper deploy script

---
+++ full_deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+VERSION=$1
+
+if [! -z "$(git status --porcelain)" ]
+then
+	echo Working directory needs to be clean to perform this operation.
+	exit 1
+fi
+
+if [ -z "${VERSION}" ]
+then
+	echo Expected version as the first parameter.
+	exit 1
+fi
+
+echo Building...
+./build.sh
+
+if [-z "$(git status --porcelain)" ]
+then
+	echo No changes to deploy. Aborting.
+	exit 1
+fi
+
+echo Pushing dist and tags...
+git add -A .
+git commit -m "Deploying ${VERSION}"
+git tag ${VERSION}
+git push --tags
+
+echo Deploying...
+./deploy.sh
